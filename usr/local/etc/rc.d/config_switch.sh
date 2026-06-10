a434b9c60677a6e4d45cd7d5930b10b5b34a530e
bfarnam <brett_farnam@yahoo.com>
2026-06-09 19:50:43 -0500

    add dual configs

--- usr/local/etc/rc.d/config_switch.sh
+++ usr/local/etc/rc.d/config_switch.sh
@@ -6,6 +6,8 @@
 # /usr/local/etc/rc.d/configure_switch.sh
 # chmod +x /usr/local/etc/rc.d/configure_switch.sh
 
+# IMPORTANT - The port configuration MUST MATCH what is in rc.conf!
+
 # PROVIDE: config_switch
 # REQUIRE: FILESYSTEMS
 # BEFORE:  NETWORKING
@@ -20,6 +22,7 @@ config_switch_start() {
     
     etherswitchcfg config vlan_mode DOT1Q
     
+    # This is the 4x4 configuration where 4 ports are bound to ix0 and 4 ports are bound to ix1
     etherswitchcfg vlangroup1 vlan 110 members 1,9t
     etherswitchcfg vlangroup2 vlan 120 members 2,9t
     etherswitchcfg vlangroup3 vlan 130 members 3,9t
@@ -28,7 +31,17 @@ config_switch_start() {
     etherswitchcfg vlangroup6 vlan 160 members 6,10t
     etherswitchcfg vlangroup7 vlan 170 members 7,10t
     etherswitchcfg vlangroup8 vlan 180 members 8,10t
-    
+
+    # This is the standard firewall 1x7 config where 1 port is bound to ix0 and the remaining are bound to ix1
+    # etherswitchcfg vlangroup1 vlan 110 members 1,9t
+    # etherswitchcfg vlangroup2 vlan 120 members 2,10t
+    # etherswitchcfg vlangroup3 vlan 130 members 3,10t
+    # etherswitchcfg vlangroup4 vlan 140 members 4,10t
+    # etherswitchcfg vlangroup5 vlan 150 members 5,10t
+    # etherswitchcfg vlangroup6 vlan 160 members 6,10t
+    # etherswitchcfg vlangroup7 vlan 170 members 7,10t
+    # etherswitchcfg vlangroup8 vlan 180 members 8,10t
+
     etherswitchcfg port1 pvid 110
     etherswitchcfg port2 pvid 120
     etherswitchcfg port3 pvid 130
